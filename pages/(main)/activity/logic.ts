d4bac43a7747210d9f7a11d2dece7802d5cecb38
ailtian <ailtian2025@gmail.com>
2025-11-08 11:27:21 +0400

    fix: Actions

--- pages/(main)/activity/logic.ts
+++ pages/(main)/activity/logic.ts
@@ -1,27 +1,27 @@
-import ActivityList from './segments/list/index.vue'
-import Unclaimed from './segments/unclaimed/index.vue'
+import ActivityList from "./segments/list/index.vue"
+import Unclaimed from "./segments/unclaimed/index.vue"
 
-export const useActivityLogic = () => {
-	const { t } = useI18n()
+export function useActivityLogic() {
+  const { t } = useI18n()
 
-	const isHasUnclaimed = ref(false)
+  const isHasUnclaimed = ref(false)
 
-	const PageType = {
-		ActivityList: 1,
-		Rebate: 2,
-		Vip: 3,
-		Redeem: 4,
-		Claim: 5,
-		Unclaimed: 6,
-		TaskList: 7,
-	}
+  const PageType = {
+    ActivityList: 1,
+    Rebate: 2,
+    Vip: 3,
+    Redeem: 4,
+    Claim: 5,
+    Unclaimed: 6,
+    TaskList: 7,
+  }
 
-	const tabList = computed(() => [
-		{ type: 'activityList', component: ActivityList, value: PageType.ActivityList, name: `${t('activity.events')}`, isShow: true, isShowRedPoint: false },
-		{ type: 'unclaimed', component: Unclaimed, value: PageType.Unclaimed, name: `${t('activity.unclaimed')}`, isShow: true, isShowRedPoint: isHasUnclaimed.value },
-	])
+  const tabList = computed(() => [
+    { type: "activityList", component: ActivityList, value: PageType.ActivityList, name: `${t("activity.events")}`, isShow: true, isShowRedPoint: false },
+    { type: "unclaimed", component: Unclaimed, value: PageType.Unclaimed, name: `${t("activity.unclaimed")}`, isShow: true, isShowRedPoint: isHasUnclaimed.value },
+  ])
 
-	return {
-		tabList
-	}
+  return {
+    tabList,
+  }
 }
